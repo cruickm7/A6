89bd2b1607cd65c212fbb2139c320d6284eb4f64
cruickm7 <60183191+cruickm7@users.noreply.github.com>
2021-03-25 16:54:46 -0300

    Final

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
 #include "cStudent.h"
 using namespace std;
 int main() {
-    cFaculty faculty;
-    cStaff staff;
-    cStudent student("qwer","asdf", "ZXCv", "tyui", "hjkl");
-//    cin >> staff;
-    cout << student;
+    cFaculty faculty("Homer Simpson", "742 Evergreen Terrace", "555-3226", "Homer.Simpson@aol.com", "Springfield Nuclear Power PLant", "$24,395", "Episode 3", "Sector 7G", "9-5", "Nuclear Safety Inspector");
+    cStaff staff("Ned Flanders", "744 Evergreen Terrace", "555-8904", "ned.flanders@springface.com", "The Leftorium", "$300,000", "Episode 38", "Owner");
+    cStudent student("Bart Simpson","742 Evergreen Terrace", "555-6832", "ElBarto@hotmail.com", "Grade 3");
+    cout << faculty << endl;
+    cout << staff << endl;
+    cout << student << endl;
     return 0;
 }
